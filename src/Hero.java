184d6063ee39b1f56fa5592da779f53b4e643576
sarahzhang10 <sarah_zhang@student.wayland.k12.ma.us>
2017-03-16 08:40:18 -0400

    added the stuff in Hero class

--- src/Hero.java
+++ src/Hero.java
@@ -1,5 +1,20 @@
+import java.awt.*;
+
 /**
- * Created by clarissa_briasco on 3/16/17.
+ * Created by sarah_zhang on 3/15/17.
  */
 public class Hero {
+
+    private int x, y;
+
+    public Hero(int xx, int yy){
+        x = xx;
+        y = yy;
+    }
+
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.BLACK);
+        g2.drawRect(x, y, 30, 50);
+    }
+
 }
